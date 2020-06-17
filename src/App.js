ac62a16ce731ad9078d94c0c37c1c3ebbe56e41a
Ubuntu <ubuntu@ip-172-31-16-212.ap-southeast-1.compute.internal>
2020-06-17 05:37:36 +0000

    更新到第28章

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
+import Title from './Title';
 import profile from './assets/profile.png';
 
 class App extends Component {
@@ -9,38 +10,33 @@ class App extends Component {
         this.setState({ displayBio: !this.state.displayBio });
     }
     render() {
-        return (
-		      <div>
-		        <img src={profile} alt="profile" className ='profile'/>
-						<h1>Hello!</h1>
-						<p>My name is David. I am a software engineer.</p>
-						<p>I am always looking forward to working on meaningful projects.</p>
-						<div>
-							<p>I live in San Francisco, and code every day.</p>
-							<p>My favorite language is Javascript</p>
-							<p>Besides coding, I also love music and ramen!</p>
-
-		          {
-		            this.state.displayBio ? (
-		              <div>
-		                <p>I am ryowu.</p>
-		                <p>43 year old.</p>
-		                <p>Live in Tainan.</p>
-		                <button onClick={this.toggleDisplayBio}>Read Less</button>
-		              </div>
-		            ) : (
-		              <div>
-		                <button onClick={this.toggleDisplayBio}>Read More</button>
-		              </div>
-		            )
-		          }
-		          <hr/>
-		          <Projects />
-							<hr/>
-		          <SocialProfiles/>
-		        </div>
-		      </div>
-        )
+			return (
+				<div>
+					<img src={profile} alt="profile" className ='profile'/>
+					<h1>Hello!</h1>
+					<p>My name is Ryowu.</p>
+					<Title />
+					<p>I am always looking forward to working on meaningful projects.</p>
+					{
+						this.state.displayBio ? (
+							<div>
+								<p>I live in San Francisco, and code every day.</p>
+								<p>My favorite language is Javascript</p>
+								<p>Besides coding, I also love music and ramen!</p>
+									<button onClick={this.toggleDisplayBio}>Read Less</button>
+							</div>
+						) : (
+							<div>
+								<button onClick={this.toggleDisplayBio}>Read More</button>
+							</div>
+						)
+					}
+					<hr/>
+					<Projects />
+					<hr/>
+					<SocialProfiles/>
+				</div>
+			)
     }
 }
 
