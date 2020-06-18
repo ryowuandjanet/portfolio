c6ec3484e095056f0038e11df250559212ed6ec6
Ubuntu <ubuntu@ip-172-31-16-212.ap-southeast-1.compute.internal>
2020-06-18 01:13:54 +0000

    更新到第34章

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
 import Title from './Title';
-import Jokes from './Jokes';
 import profile from '../assets/profile.png';
 
 class App extends Component {
@@ -36,8 +35,6 @@ class App extends Component {
 					<Projects />
 					<hr/>
 					<SocialProfiles/>
-					<hr/>
-					<Jokes />
 				</div>
 			)
     }
